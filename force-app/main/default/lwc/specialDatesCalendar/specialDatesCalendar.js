74e1fc19d43b7805b5be86ac557e410e4327d169
Marcelo Costa <marcelo.cost@marceloco-ltm.internal.salesforce.com>
2021-01-08 10:15:36 +1100

    commit to public repo

--- force-app/main/default/lwc/specialDatesCalendar/specialDatesCalendar.js
+++ force-app/main/default/lwc/specialDatesCalendar/specialDatesCalendar.js
@@ -13,7 +13,8 @@ export default class SpecialDatesCalendar extends LightningElement {
   @api recordId;
   @api sObjectType;
   @track isMomentLoaded = false;
-
+  // eslint-disable-next-line no-use-before-define
+  @track calendar;
   dateNodes = [];
   currentDate;
   selectedDate;
@@ -123,5 +124,87 @@ export default class SpecialDatesCalendar extends LightningElement {
       }
     }
     e.target.classList.toggle("selected");
+  }
+}
+
+class Calendar {
+  dateNodes = [];
+  currentDate;
+
+  constructor() {
+    this.currentDate = moment().startOf("month");
+    this.setDateNodesForCurrentDate();
+    this.gotoNextMonth = this.gotoNextMonth.bind(this);
+    this.gotoPrevMonth = this.gotoPrevMonth.bind(this);
+    this.gotoToday = this.gotoToday.bind(this);
+    this.setDateNodesForCurrentDate = this.setDateNodesForCurrentDate.bind(
+      this
+    );
+    Object.defineProperty(this, "month", {
+      get: function () {
+        return this.currentDate.format("MMMM");
+      }.bind(this)
+    });
+    Object.defineProperty(this, "year", {
+      get: function () {
+        return this.currentDate.format("YYYY");
+      }.bind(this)
+    });
+    this.dateNodes = [];
+    this.setDateNodesForCurrentDate();
+  }
+
+  gotoNextMonth() {
+    debugger;
+    this.currentDate.add(1, "months").startOf("month");
+    this.setDateNodesForCurrentDate();
+  }
+
+  gotoPrevMonth() {
+    debugger;
+    this.currentDate.subtract(1, "months").startOf("month");
+    this.setDateNodesForCurrentDate();
+  }
+
+  gotoToday() {
+    this.currentDate = moment().startOf("month");
+    debugger;
+    this.setDateNodesForCurrentDate();
+  }
+
+  setDateNodesForCurrentDate() {
+    // reset dates variable
+    this.dateNodes = [];
+    // we have to clone the var due to the mutability of the moment variable.
+    let iterator = this.currentDate.clone();
+    let startOfTheMonth = this.currentDate.clone();
+    let endOfTheMonth = this.currentDate.clone().endOf("month");
+    //Not the best approach but it works.
+    while (iterator.isoWeekday() != 7) {
+      iterator.add(-1, "days");
+    }
+    //here we ensure that the iterator goes until first saturday after the end of the month
+    while (
+      moment(iterator.format("YYYY-MM-DD")).isBefore(
+        endOfTheMonth.format("YYYY-MM-DD")
+      ) ||
+      iterator.isoWeekday() != 7
+    ) {
+      //Constructing the node... TODO:Decouple this logic.
+      let className = "date";
+      //If not the same month, should be a padder
+      if (iterator.format("MM") != startOfTheMonth.format("MM")) {
+        className = "padder";
+      }
+      let formatted = iterator.format("YYYY-MM-DD");
+      let text = iterator.format("DD");
+      this.dateNodes.push({
+        className,
+        formatted,
+        text
+      });
+      //add 1 to Iterator date
+      iterator.add(1, "days");
+    }
   }
 }
